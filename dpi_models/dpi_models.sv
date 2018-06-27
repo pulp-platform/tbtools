06a31a41b90981ee5066e6559b7570c6e9d0edfe
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-06-28 00:42:10 +0200

    Removed few warnings

--- dpi_models/dpi_models.sv
+++ dpi_models/dpi_models.sv
@@ -164,6 +164,8 @@ package dpi_models;
 
 
     task jtag_bind(string name, virtual JTAG jtag_itf);
+      chandle dpi_context;
+
       jtag_itf.tck = 'b1;
       jtag_itf.tdi = 'b1;
       jtag_itf.tms = 'b1;
@@ -173,11 +175,13 @@ package dpi_models;
       jtag_itf_array = new[nb_jtag_itf](jtag_itf_array);
       jtag_itf_array[nb_jtag_itf - 1] = jtag_itf;
 
-      dpi_jtag_bind(dpi_model, name, nb_jtag_itf - 1);
+      dpi_context = dpi_jtag_bind(dpi_model, name, nb_jtag_itf - 1);
     endtask
 
 
     task ctrl_bind(string name, virtual CTRL ctrl_itf);
+      chandle dpi_context;
+
       $display("[TB] %t - SETTING RESET TO 1", $realtime);
       ctrl_itf.reset = 'b1;
 
@@ -185,7 +189,7 @@ package dpi_models;
       ctrl_itf_array = new[nb_ctrl_itf](ctrl_itf_array);
       ctrl_itf_array[nb_ctrl_itf - 1] = ctrl_itf;
 
-      dpi_ctrl_bind(dpi_model, name, nb_ctrl_itf - 1);
+      dpi_context = dpi_ctrl_bind(dpi_model, name, nb_ctrl_itf - 1);
     endtask
 
 
--- tb_driver/tb_driver.sv
+++ tb_driver/tb_driver.sv
@@ -49,19 +49,19 @@ package tb_driver;
     jtag_info_t jtag_infos[];
     ctrl_info_t ctrl_infos[];
 
-    function register_qspim_itf(int itf_id, virtual QSPI itf, virtual QSPI_CS cs[]);
+    function void register_qspim_itf(int itf_id, virtual QSPI itf, virtual QSPI_CS cs[]);
       qspi_infos = new[itf_id+1] (qspi_infos);
       qspi_infos[itf_id].itf = itf;
       qspi_infos[itf_id].cs = cs;
       //qspi_infos[itf_id].cs = new[cs.size] (cs);
     endfunction
 
-    function register_jtag_itf(int itf_id, virtual JTAG itf);
+    function void register_jtag_itf(int itf_id, virtual JTAG itf);
       jtag_infos = new[itf_id+1] (jtag_infos);
       jtag_infos[itf_id].itf = itf;
     endfunction
 
-    function register_ctrl_itf(int itf_id, virtual CTRL itf);
+    function void register_ctrl_itf(int itf_id, virtual CTRL itf);
       ctrl_infos = new[itf_id+1] (ctrl_infos);
       ctrl_infos[itf_id].itf = itf;
     endfunction
